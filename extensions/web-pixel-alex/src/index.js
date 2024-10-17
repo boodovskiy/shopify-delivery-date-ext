ab0fda237cd3692dbf2935b5a7e0fe3badc252c7
boodovskiy <dzak1987@mail.ru>
2024-10-17 09:58:29 +0300

    Change subscriptions on all_events, add scopes for pixel

--- extensions/web-pixel-alex/src/index.js
+++ extensions/web-pixel-alex/src/index.js
@@ -2,9 +2,9 @@ import {register} from "@shopify/web-pixels-extension";
 
 register(({ analytics, browser, init, settings }) => {
     // Bootstrap and insert pixel script tag here
-
+    console.log('alex pixel')
     // Sample subscribe to page view
-    analytics.subscribe('page_viewed', (event) => {
+    analytics.subscribe('all_events', (event) => {
       console.log('Page viewed', event);
     });
 });

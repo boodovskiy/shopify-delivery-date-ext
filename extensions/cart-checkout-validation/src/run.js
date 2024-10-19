e5406ca3d14f30e52344034ab0a605ebdcfd2f78
boodovskiy <dzak1987@mail.ru>
2024-10-19 10:27:07 +0300

    Add validation rule for cart

--- extensions/cart-checkout-validation/src/run.js
+++ extensions/cart-checkout-validation/src/run.js
@@ -10,14 +10,26 @@
  * @returns {FunctionRunResult}
  */
 export function run(input) {
-  const errors = input.cart.lines
-    .filter(({ quantity }) => quantity > 1)
-    .map(() => ({
-      localizedMessage: "Not possible to order more than one of each",
-      target: "$.cart",
-    }));
+  // The error
+  const error = {
+    localizedMessage:
+        "There is an order maximum of $500,00 for customers without established order history",
+    target: "cart"
+  };
+  // Parse the decimal (serialized as a string) into a float.
+  const orderSubtotal = parseFloat(input.cart.cost.subtotalAmount.amount);
+  const errors = [];
 
-  return {
-    errors
+  // Orders with subtotals greater than $1,000 are available only to established customers.
+  if (orderSubtotal > 500.0) {
+    // If the customer has ordered less than 5 times in the past,
+    // then treat them as a new customer.
+    const numberOfOrders = input.cart.buyerIdentity?.customer?.numberOfOrders ?? 0;
+
+    if (numberOfOrders < 5) {
+      errors.push(error);
+    }
   }
+
+  return { errors };
 };

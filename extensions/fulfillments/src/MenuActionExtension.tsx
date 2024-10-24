d2328942a66061d84bf7f6ab26cfbd3df7c8881d
boodovskiy <dzak1987@mail.ru>
2024-10-24 11:21:07 +0300

    Test scopes and Menu Action Extensions

--- extensions/fulfillments/src/MenuActionExtension.tsx
+++ extensions/fulfillments/src/MenuActionExtension.tsx
@@ -5,49 +5,55 @@ import {
 } from "@shopify/ui-extensions-react/customer-account";
 
 export default reactExtension(
-  "customer-account.order.action.menu-item.render",
-  async (api) => {
-    console.log("test 1");
-    const { orderId } = api;
-    let hasFulfillments = false;
-    try {
-      const orderQuery = {
-        query: `query {
-          order(id: "${orderId}") {
-            fulfillments(first: 1) {
-              nodes {
-                latestShipmentStatus
-              }
-            }
-          }
-        }`,
-      };
-      const result = await fetch(
-        "shopify://customer-account/api/unstable/graphql.json",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(orderQuery),
-        }
-      );
-      const { data } = await result.json();
-      hasFulfillments = data.order.fulfillments.nodes.length !== 0;
-      console.log(`${hasFulfillments}`, data);
-    } catch (error) {
-      console.log(error);
-      hasFulfillments = false;
-    }
-    return <MenuActionExtension showAction={hasFulfillments} />;
-  }
-);
+  "customer-account.order.action.menu-item.render", () => (
+    <MenuActionExtension />
 
-function MenuActionExtension({ showAction }: { showAction: boolean }) {
-    console.log("test 2");
-  if (!showAction) {
-    return null;
-  }
+//   async (api) => {
+//     console.log("test 1");
+//     const { orderId } = api;
+//     let hasFulfillments = false;
+//     try {
+//       const orderQuery = {
+//         query: `query {
+//           order(id: "${orderId}") {
+//             fulfillments(first: 1) {
+//               nodes {
+//                 latestShipmentStatus
+//               }
+//             }
+//           }
+//         }`,
+//       };
+//       const result = await fetch(
+//         "shopify://customer-account/api/unstable/graphql.json",
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify(orderQuery),
+//         }
+//       );
+//       const { data } = await result.json();
+//       hasFulfillments = data.order.fulfillments.nodes.length !== 0;
+//       console.log(`${hasFulfillments}`, data);
+//     } catch (error) {
+//       console.log(error);
+//       hasFulfillments = false;
+//     }
+//     return <MenuActionExtension showAction={hasFulfillments} />;
+//   }
+));
 
-  return <Button>Report a problem</Button>;
-}
+// function MenuActionExtension({ showAction }: { showAction: boolean }) {
+//   if (!showAction) {
+//     return null;
+//   }
+
+//   return <Button>Report a problem</Button>;
+// }
+
+function MenuActionExtension() {
+    return "Hello world";
+  }
+  
